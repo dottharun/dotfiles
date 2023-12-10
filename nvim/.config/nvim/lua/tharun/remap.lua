dc28d88f84dbcd9d24fa0b96fed77fb0bff3972f
ubutharun <tharun242424@gmail.com>
2023-12-11 01:21:53 +0530

    jk remap

--- nvim/.config/nvim/lua/tharun/remap.lua
+++ nvim/.config/nvim/lua/tharun/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+local options = { noremap = true }
+vim.keymap.set("i", "jj", "<Esc>", options)
+vim.keymap.set("i", "jk", "<Esc>", options)
+vim.keymap.set("i", "kj", "<Esc>", options)
