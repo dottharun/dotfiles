304448f03d9754546657ca1d2fbeeabbffb0aa12
ubutharun <tharun242424@gmail.com>
2023-12-22 00:07:36 +0530

    tex snippet action

--- nvim/.config/nvim/after/ftplugin/tex.lua
+++ nvim/.config/nvim/after/ftplugin/tex.lua
@@ -2,8 +2,8 @@
 
 local options = { noremap = true }
 --save, compile, view
-vim.keymap.set("n", "<leader>l", "<Cmd>update<CR><Cmd>VimtexCompileSS<CR>", options)
---vim.keymap.set("n", "<leader>k", "<Cmd>VimtexStop<CR>", options)
+vim.keymap.set("n", "<leader>l", "<Cmd>update<CR><Cmd>VimtexCompile<CR><Cmd>VimtexView<CR>", options)
+vim.keymap.set("n", "<leader>k", "<Cmd>VimtexStop<CR>", options)
 vim.keymap.set("n", "<leader>c", "<Cmd>VimtexClean<CR>", options)
 vim.keymap.set("n", "<leader>v", "<Cmd>VimtexView<CR>", options)
 vim.keymap.set("n", "<leader>r", "<Cmd>VimtexErrors<CR>", options)
@@ -13,3 +13,7 @@ vim.keymap.set("n", "<leader>t", "<Cmd>VimtexTocOpen<CR>", options)
 vim.keymap.set("n", "dsm", "<Plug>(vimtex-env-delete-math)")
 vim.keymap.set("n", "csm", "<Plug>(vimtex-env-change-math)")
 vim.keymap.set("n", "tsm", "<Plug>(vimtex-env-toggle-math)")
+-- spell check
+vim.opt_local.spell = true
+vim.opt.spelllang = 'en_us'
+vim.keymap.set("i", "<C-w>", "<c-g>u<C-[>[s1z=`]a<c-g>u", options)
