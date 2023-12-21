8ac2fe088a40b0e6723b7cedb6db8b5a09ffdcf7
ubutharun <tharun242424@gmail.com>
2023-12-21 10:36:02 +0530

    tex lsp and correction

--- nvim/.config/nvim/after/ftplugin/tex.lua
+++ nvim/.config/nvim/after/ftplugin/tex.lua
@@ -8,3 +8,8 @@ vim.keymap.set("n", "<leader>c", "<Cmd>VimtexClean<CR>", options)
 vim.keymap.set("n", "<leader>v", "<Cmd>VimtexView<CR>", options)
 vim.keymap.set("n", "<leader>r", "<Cmd>VimtexErrors<CR>", options)
 vim.keymap.set("n", "<leader>t", "<Cmd>VimtexTocOpen<CR>", options)
+
+-- change delete toggle
+vim.keymap.set("n", "dsm", "<Plug>(vimtex-env-delete-math)")
+vim.keymap.set("n", "csm", "<Plug>(vimtex-env-change-math)")
+vim.keymap.set("n", "tsm", "<Plug>(vimtex-env-toggle-math)")
--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -2,8 +2,8 @@ local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(_, bufnr)
     print("help me lsp")
-    local opts = { buffer = bufnr, remap = false }
 
+    local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -18,7 +18,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = { 'tsserver', 'eslint', 'rust_analyzer', 'lua_ls', 'clangd', 'gopls' },
+    ensure_installed = { 'texlab', 'tsserver', 'eslint', 'rust_analyzer', 'lua_ls', 'clangd', 'gopls' },
 
     handlers = {
         lsp_zero.default_setup,
@@ -31,7 +31,6 @@ require('mason-lspconfig').setup({
                             checkThirdParty = false,
                             library = {
                                 vim.fn.expand("$VIMRUNTIME/lua"),
-                                --vim.fn.stdpath('config') .. '/lua'
                             },
                         },
                     },
@@ -40,6 +39,9 @@ require('mason-lspconfig').setup({
 
             require("lspconfig").lua_ls.setup(lua_opts)
         end,
+        texlab = function()
+            require("lspconfig").texlab.setup({})
+        end
     },
 })
 
