1057592fca309922b0a581d1fdd0bca4b3885b6e
ubutharun <tharun242424@gmail.com>
2024-01-03 20:08:21 +0530

    colors

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -40,6 +40,18 @@ function Tokyo()
 end
 
 function Kana()
+    require('kanagawa').setup({
+        colors = {
+            theme = {
+                all = {
+                    ui = {
+                        bg_gutter = "none"
+                    }
+                }
+            }
+        }
+    })
+
     vim.cmd("colorscheme kanagawa")
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
