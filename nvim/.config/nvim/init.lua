a1986ad963667187b300c056074f0892dcf4eeff
ubutharun <tharun242424@gmail.com>
2024-01-03 20:06:42 +0530

    autocommands for yank highlight and auto trailing whitespace removal

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,29 @@
 require("tharun")
 -- print("hello")
+
+local augroup = vim.api.nvim_create_augroup
+local AutoRemoveTrailingWhiteSpaceGroup = augroup('whiteSpace', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({ "BufWritePre" }, {
+    group = AutoRemoveTrailingWhiteSpaceGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
