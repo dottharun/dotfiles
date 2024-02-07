15f1388239f22627970975ca2a6a92eecf7eced6
Tharun T <tharun242424@gmail.com>
2024-02-07 18:44:52 +0530

    arrows with ctrl hjkl

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -5,11 +5,19 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<C-m>", function() harpoon:list():append() end)
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = "add buffer to harpoon" })
+vim.keymap.set("n", "<leader>a", function()
+    print("added to harpoon")
+    harpoon:list():append()
+end, { desc = "add buffer to harpoon" })
 
-vim.keymap.set("n", "<leader>t", function()
+vim.keymap.set("n", "<c-e>", function()
     harpoon.ui:toggle_quick_menu(harpoon:list())
 end, { desc = "open harpoon tab list" })
+
+-- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = "goto harpoon - 1" })
+-- vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, { desc = "goto harpoon - 2" })
+-- vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end, { desc = "goto harpoon - 3" })
+-- vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end, { desc = "goto harpoon - 4" })
 
 vim.keymap.set("n", "<leader>h", function() harpoon:list():select(1) end, { desc = "goto harpoon - 1" })
 vim.keymap.set("n", "<leader>j", function() harpoon:list():select(2) end, { desc = "goto harpoon - 2" })
