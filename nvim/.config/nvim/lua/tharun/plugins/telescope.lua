5c0403db734e7a201ef9fb220f000940e3bf0095
Tharun T <tharun242424@gmail.com>
2024-06-09 00:06:19 +0530

    fix: telescope
    Signed-off-by: Tharun T <tharun242424@gmail.com>

--- nvim/.config/nvim/lua/tharun/plugins/telescope.lua
+++ nvim/.config/nvim/lua/tharun/plugins/telescope.lua
@@ -19,22 +19,20 @@ return {
 		})
 
 		local builtin = require("telescope.builtin")
-
 		local options = { noremap = true }
 
 		-- think folder means project thus folder find, folder grep
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, options)
+		vim.keymap.set("n", "<leader>fl", builtin.live_grep, options)
+
+		-- find grep and search
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, options)
+		vim.keymap.set("n", "<leader>fs", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
 
+		-- find telescope
 		vim.keymap.set("n", "<leader>ft", builtin.builtin, options)
-
-		-- for searching relative to current buffer dir
-		vim.keymap.set("n", "<leader>fbf", function()
-			builtin.find_files({ cwd = vim.fn.expand("%:p:h") })
-		end, options)
-		vim.keymap.set("n", "<leader>fbg", function()
-			builtin.live_grep({ cwd = vim.fn.expand("%:p:h") })
-		end, options)
 
 		vim.keymap.set("n", "<C-p>", builtin.git_files, options)
 		vim.keymap.set("n", "<leader>fbb", builtin.buffers, {})
