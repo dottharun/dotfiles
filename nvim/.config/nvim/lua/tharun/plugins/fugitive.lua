4e0c9c8b3fb9cd8a166c31e7f9e8d985a716a62f
Tharun T <tharun242424@gmail.com>
2024-06-13 18:39:19 +0530

    fugit

--- nvim/.config/nvim/lua/tharun/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/tharun/plugins/fugitive.lua
@@ -21,12 +21,7 @@ return {
 		vim.keymap.set("n", "<leader>ga", ":Git add .<CR>", { desc = "[g]it [a]dd ." })
 		vim.keymap.set("n", "<leader>gcs", ":Git commit -s<CR>", { desc = "[g]it [c]ommit --[s]igned" })
 
-		vim.keymap.set("n", "<leader>gdd", ":Git diff<CR>", { desc = "[g]it [d]iff for only curr buff" })
-		vim.keymap.set(
-			"n",
-			"<leader>gdt",
-			":Git difftool<CR>",
-			{ desc = "[g]it [d]iff [t]ool for project as qf-hunks" }
-		)
+		vim.keymap.set("n", "<leader>gd", ":Git diff<CR>", { desc = "[g]it [d]iff for only curr buff" })
+		vim.keymap.set("n", "<leader>gt", ":Git difftool<CR>", { desc = "[g]it diff [t]ool for project as qf-hunks" })
 	end,
 }
