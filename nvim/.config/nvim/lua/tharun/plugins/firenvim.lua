6eb29879f0569af0e9c8249b45fd6eb941cdc281
Tharun T <tharun242424@gmail.com>
2024-09-01 05:53:47 +0530

    firenvim resizing and enter

--- nvim/.config/nvim/lua/tharun/plugins/firenvim.lua
+++ nvim/.config/nvim/lua/tharun/plugins/firenvim.lua
@@ -18,8 +18,36 @@ return {
             vim.o.guifont = "Source Code Pro:h14"
             vim.o.laststatus = 0 -- to stop showing status lines
 
+            -- Hack to set the min-lines in loop - since browser resizes in all the time
+            local timer = vim.uv.new_timer()
+            timer:start(
+                0,
+                500,
+                vim.schedule_wrap(function()
+                    if vim.o.lines < 6 then
+                        vim.o.lines = 6
+                    end
+                end)
+            )
+
             -- to go back to website
-            vim.api.nvim_set_keymap("n", "<Esc><Esc>", "<Cmd>call firenvim#focus_page()<CR>", {})
+            vim.keymap.set("n", "<Esc>", "<Cmd>call firenvim#focus_page()<CR>", {})
+            -- hide firenvim
+            vim.keymap.set("n", "<C-z>", "<Cmd>call firenvim#hide_frame()<CR>", {})
+            -- send chat+enter to browser
+            vim.keymap.set(
+                "n",
+                "<leader><CR>",
+                vim.schedule_wrap(function()
+                    -- print("pressing enter key")
+                    vim.cmd([[
+                    silent write
+                    call firenvim#press_keys('<CR>')
+                    ]])
+                    vim.api.nvim_feedkeys("ggdG", "n", false)
+                end),
+                {}
+            )
 
             -- website specific commands
             vim.api.nvim_create_autocmd({ "BufEnter" }, {
