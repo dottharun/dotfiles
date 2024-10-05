abcf621516dcb516893f9cd41c008199bf6cd5d6
Tharun T <tharun242424@gmail.com>
2024-10-06 03:48:08 +0530

    tree

--- nvim/.config/nvim/lua/tharun/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/tharun/plugins/treesitter.lua
@@ -39,7 +39,7 @@ return {
                 highlight = {
                     -- `false` will disable the whole extension
                     enable = true,
-                    disable = { "latex", "csv" },
+                    disable = { "latex", "csv", "swift" },
 
                     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
                     -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
