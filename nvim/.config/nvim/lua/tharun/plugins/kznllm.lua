798465f8eceb0ee2051736de479506a649dbb49a
Tharun T <tharun242424@gmail.com>
2024-10-08 11:53:50 +0530

    kzn & tree upgrade

--- nvim/.config/nvim/lua/tharun/plugins/kznllm.lua
+++ nvim/.config/nvim/lua/tharun/plugins/kznllm.lua
@@ -2,37 +2,101 @@ return {
     "chottolabs/kznllm.nvim",
     dependencies = {
         { "nvim-lua/plenary.nvim" },
+        { "chottolabs/plenary.nvim" }, -- patched to resolve symlinked directories
     },
+    dev = true,
     config = function(self)
-        local kznllm = require("kznllm")
-        local spec = require("kznllm.specs.openai")
+        local presets = require("kznllm.presets")
 
-        kznllm.TEMPLATE_DIRECTORY = vim.fn.expand(self.dir) .. "/templates/"
-        -- fallsback to a preset default model configuration
+        vim.keymap.set({ "n", "v" }, "<leader>zs", presets.switch_presets, { desc = "switch between presets" })
 
-        local function groq_llm()
-            kznllm.invoke_llm({
-                { role = "system", prompt_template = spec.PROMPT_TEMPLATES.GROQ.FILL_MODE_SYSTEM_PROMPT },
-                { role = "user", prompt_template = spec.PROMPT_TEMPLATES.GROQ.FILL_MODE_USER_PROMPT },
-            }, spec.make_job)
+        local function llm_fill()
+            local spec, preset = presets.load()
+            presets.invoke_llm(
+                preset.make_data_fn,
+                spec.make_curl_args,
+                spec.make_job,
+                vim.tbl_extend("keep", preset.opts, {})
+            )
         end
 
-        vim.keymap.set({ "n", "v" }, "<leader>zq", groq_llm, { desc = "Send current selection to LLM groq_llm" })
-        vim.keymap.set({ "n", "v" }, "<C-q>", groq_llm, { desc = "Send current selection to LLM groq_llm" })
+        vim.keymap.set({ "n", "v" }, "<leader>zz", llm_fill, { desc = "Send current selection to LLM llm_fill" })
 
-        -- openrouter nous hermes
-        spec.SELECTED_MODEL = { name = "nousresearch/hermes-3-llama-3.1-405b" }
-        spec.API_KEY_NAME = "OPEN_ROUTER_API_KEY"
-        spec.URL = "https://openrouter.ai/api/v1/chat/completions"
+        -- optional for debugging purposes
+        local function debug()
+            local spec, preset = presets.load()
+            presets.invoke_llm(
+                preset.make_data_fn,
+                spec.make_curl_args,
+                spec.make_job,
+                vim.tbl_extend("keep", preset.opts, { debug = true })
+            )
+        end
+
+        vim.keymap.set({ "n", "v" }, "<leader>zd", debug, { desc = "Send current selection to LLM debug" })
+
+        vim.api.nvim_set_keymap("n", "<Esc>", "", {
+            noremap = true,
+            silent = true,
+            callback = function()
+                vim.api.nvim_exec_autocmds("User", { pattern = "LLM_Escape" })
+            end,
+        })
+
+        local kznllm = require("kznllm")
+        local Path = require("plenary.path")
+
+        local TEMPLATE_DIRECTORY = Path:new(vim.fn.expand(self.dir) .. "/templates")
+
+        ---Example implementation of a `make_data_fn` compatible with `kznllm.invoke_llm` for groq spec
+        ---@param prompt_args any
+        ---@param opts { model: string, temperature: number, template_directory: Path, debug: boolean }
+        ---@return table
+        ---
+        local function make_data_for_hermes_chat(prompt_args, opts)
+            return {
+                messages = {
+                    {
+                        role = "system",
+                        content = kznllm.make_prompt_from_template(
+                            opts.template_directory / "nous_research/fill_mode_system_prompt.xml.jinja",
+                            prompt_args
+                        ),
+                    },
+                    {
+                        role = "user",
+                        content = kznllm.make_prompt_from_template(
+                            opts.template_directory / "nous_research/fill_mode_user_prompt.xml.jinja",
+                            prompt_args
+                        ),
+                    },
+                },
+                model = opts.model,
+                temperature = opts.temperature,
+                stream = true,
+            }
+        end
+
+        -- set initial preset on load
+        local spec = require("kznllm.specs.openai")
+
+        -- spec.SELECTED_MODEL = { name = "nousresearch/hermes-3-llama-3.1-405b" }
+        -- spec.API_KEY_NAME = "OPEN_ROUTER_API_KEY"
+        -- spec.URL = "https://openrouter.ai/api/v1/chat/completions"
 
         local function hermes_fill()
-            kznllm.invoke_llm({
-                { role = "system", prompt_template = spec.PROMPT_TEMPLATES.NOUS_RESEARCH.FILL_MODE_SYSTEM_PROMPT },
-                { role = "user", prompt_template = spec.PROMPT_TEMPLATES.NOUS_RESEARCH.FILL_MODE_USER_PROMPT },
-            }, spec.make_job)
+            presets.invoke_llm(make_data_for_hermes_chat, spec.make_curl_args, spec.make_job, {
+                model = "nousresearch/hermes-3-llama-3.1-405b",
+                max_tokens = 8192,
+                temperature = 0.7,
+                base_url = "https://openrouter.ai",
+                endpoint = "/api/v1/chat/completions",
+                template_directory = TEMPLATE_DIRECTORY,
+                api_key_name = "OPEN_ROUTER_API_KEY",
+                debug = true,
+            })
         end
 
         vim.keymap.set({ "n", "v" }, "<leader>zh", hermes_fill, { desc = "Send current selection to LLM hermes_fill" })
-        vim.keymap.set({ "n", "v" }, "<C-h>", hermes_fill, { desc = "Send current selection to LLM hermes_fill" })
     end,
 }
--- nvim/.config/nvim/lua/tharun/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/tharun/plugins/treesitter.lua
@@ -65,4 +65,23 @@ return {
             end, { silent = true })
         end,
     },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
