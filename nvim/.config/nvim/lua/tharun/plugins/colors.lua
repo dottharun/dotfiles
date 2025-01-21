c4300b6c621bf91eb3e8ef251b7753654cc2e948
Tharun T <tharun242424@gmail.com>
2025-01-21 11:15:53 +0530

    themes

--- nvim/.config/nvim/lua/tharun/plugins/colors.lua
+++ nvim/.config/nvim/lua/tharun/plugins/colors.lua
@@ -31,6 +31,45 @@ return {
                 require("onedark").load()
             end
             Od()
+
+            function OneLight()
+                require("onedark").setup({
+                    style = "light",
+                    code_style = {
+                        comments = "none",
+                        keywords = "none",
+                        functions = "none",
+                        strings = "none",
+                        variables = "none",
+                    },
+                    colors = {
+                        bg0 = "#ffffff",
+                        bg1 = "#f0f0f0",
+                    },
+                })
+                require("onedark").load()
+            end
+
+            vim.api.nvim_set_keymap(
+                "n",
+                "<leader>td",
+                ":hi clear | colorscheme onedark<CR>",
+                { noremap = true, silent = true }
+            )
+            vim.api.nvim_set_keymap(
+                "n",
+                "<leader>tv",
+                ":hi clear | colorscheme visual_studio_code<CR>",
+                { noremap = true, silent = true }
+            )
+
+            vim.api.nvim_set_keymap(
+                "n",
+                "<leader>tl",
+                ":hi clear | lua OneLight()<CR>",
+                { noremap = true, silent = true }
+            )
+            vim.api.nvim_set_keymap("n", "<C-c>", ":hi clear | lua OneLight()<CR>", { noremap = true, silent = true })
         end,
     },
     {
@@ -38,6 +77,27 @@ return {
         priority = 100,
         config = function()
             -- vim.cmd([[colorscheme visual_studio_code]])
+        end,
+    },
+    {
+        "nyoom-engineering/oxocarbon.nvim",
+        priority = 100,
+        config = function()
+            -- vim.cmd([[colorscheme oxocarbon]])
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        priority = 100,
+        config = function()
+            -- vim.cmd([[colorscheme tokyonight-night]])
+        end,
+    },
+    {
+        "RRethy/base16-nvim",
+        priority = 100,
+        config = function()
+            -- vim.cmd([[colorscheme tokyonight]])
         end,
     },
 }
