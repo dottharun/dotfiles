329b63166188cb098064536ed8e4d721ecc3d03d
Tharun T <tharun242424@gmail.com>
2025-07-29 23:51:51 +0530

    fidget

--- nvim/.config/nvim/lua/tharun/plugins/fidget.lua
+++ nvim/.config/nvim/lua/tharun/plugins/fidget.lua
@@ -2,5 +2,10 @@ return {
     "j-hui/fidget.nvim",
     opts = {
         -- options
+        progress = {
+            display = {
+                render_limit = 1,
+            },
+        },
     },
 }
